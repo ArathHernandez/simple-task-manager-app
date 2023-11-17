d1081dba1e3a0ccd89e0f3acbbb0b4a602fbc8bd
ArathHernandez <earathdzv14@gmail.com>
2023-11-16 21:42:14 -0600

    SimpleTaskManager

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import batmanLogo from './imagenes/batman.png'
+import Tarea from './componentes/Tarea.js';
+import TareaFormulario from './componentes/TareaFormulario.js';
+import ListaDeTareas from './componentes/ListaDeTareas.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='batman-logo-contenedor'>
+        <img
+        src={batmanLogo}
+        className='batman-logo'
+        />
+      </div>
+      <div className='tareas-lista-principal'>
+        <h1>My tasks</h1>
+        <ListaDeTareas/>
+      </div>
     </div>
   );
 }
